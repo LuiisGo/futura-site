d88f7365d19e62176762700b9b06eeb8bbc874f5
LuiisGo <88735245+LuiisGo@users.noreply.github.com>
2026-03-22 23:58:03 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import IntroOverlay from "../components/layout/IntroOverlay";
 import Hero from "../components/sections/Hero";
+import StatsBar from "../components/sections/StatsBar";
 import Problem from "../components/sections/Problem";
 import Solution from "../components/sections/Solution";
 import Services from "../components/sections/Services";
 import Methodology from "../components/sections/Methodology";
 import SocialProofSection from "../components/sections/SocialProofSection";
+import CasosDeExito from "../components/sections/CasosDeExito";
 import Sectors from "../components/sections/Sectors";
-
 import SecuritySection from "../components/sections/SecuritySection";
 import Wallet from "../components/sections/Wallet";
+import TestimonialQuote from "../components/sections/TestimonialQuote";
 import CTASection from "../components/sections/CTASection";
 import FAQ from "../components/sections/FAQ";
 
@@ -17,15 +19,17 @@ export default function HomePage() {
     <>
       <IntroOverlay />
       <Hero />
+      <StatsBar />
       <Problem />
       <Solution />
       <Services />
-         <Methodology />
+      <Methodology />
       <SocialProofSection />
+      <CasosDeExito />
       <Sectors />
-   
       <SecuritySection />
       <Wallet />
+      <TestimonialQuote />
       <CTASection />
       <FAQ />
     </>
