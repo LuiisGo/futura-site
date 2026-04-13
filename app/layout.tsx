510e3b1b5b97fc3be1bb73bbb97bba996dbab769
Luis Marroquin <luismarroquin@Luis-MacBook-Air.local>
2026-04-13 08:26:53 -0600

    feat: add Meta Pixel and fix año typos in blog content
    - Integrate Meta Pixel (ID 807344525777365) via Next.js Script afterInteractive
    - Replace standalone 'ano'/'anos' with 'año'/'años' in blog-data.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,6 +110,30 @@ export default function RootLayout({
             gtag('config', 'G-3RQ8K6MV86');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '807344525777365');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=807344525777365&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
